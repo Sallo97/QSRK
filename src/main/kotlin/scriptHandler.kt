e7d192de9722ae3d5248f2ee1d6429784bf0aa19
Sal <salerno.salvatore1997@gmail.com>
2025-03-28 15:05:14 +0100

    Wrote parseLine, a simple function  to check if the produced line is an error or an exception

--- src/main/kotlin/scriptHandler.kt
+++ src/main/kotlin/scriptHandler.kt
@@ -3,12 +3,10 @@
  * the process's termination.
  */
 
-import java.io.IOException
-
 /**
  * Handles the execution of the Kotlin's script pointed by [source].
  */
-fun executeScript(source: String){
+fun executeScript(source: String) {
     // Initialising process
     val processScript = ProcessBuilder("kotlinc", "-script", source)
         .redirectErrorStream(true)
@@ -17,14 +15,40 @@ fun executeScript(source: String){
 
     // Reading the output stream
     val outputReader = processScript.inputReader()
+    val line = StringBuilder()
 
     do {
         val nextVal = outputReader.read()
-        print(nextVal)
-    }while (nextVal != -1)
+        val nextChar = nextVal.toChar()
+
+        print(nextChar) // TODO line is here only for debug purposes, remember to remove it.
+        line.append(nextChar)
+
+        // Checking line
+        if (nextChar == '\n' || nextChar == '\r') {
+            parseLine(line.toString())
+            line.clear()
+        }
+
+    } while (nextVal != -1)
 }
 
-fun main(){
+/**
+ * Checks if [line] is an error or an exception.
+ */
+fun parseLine(line: String) {
+    val errorRegex = Regex("(\\w*/*)*\\w+\\.kts:\\d+:\\d+: error: .*([\\n\\r])*")
+    val exceptionRegex = Regex("java\\.lang\\..+: .+([\\n\\r])*")
+
+    if (errorRegex.matches(line)) {
+        println("FOUND AN ERROR!")
+    } else if (exceptionRegex.matches(line)) {
+        println("FOUND AN EXCEPTION!")
+    }
+
+}
+
+fun main() {
     val scriptPath = "test.kts"
     executeScript(scriptPath)
 }
