8380247949b19e4b5fbd69ced1f514688eb00f36
Sal <salerno.salvatore1997@gmail.com>
2025-03-28 15:32:45 +0100

    Replaced the do-while for reading the buffer with a more clear while. Created the enum LineType to better handle the parsing of lines

--- src/main/kotlin/scriptHandler.kt
+++ src/main/kotlin/scriptHandler.kt
@@ -6,6 +6,11 @@ import java.io.IOException
  */
 
 /**
+ * Describes the type of line, that is if it is an error, exception or a normal line.
+ */
+enum class LineType { ERROR, EXCEPTION }
+
+/**
  * Handles the execution of the Kotlin's script pointed by [source].
  */
 fun executeScript(source: String) {
@@ -17,26 +22,29 @@ fun executeScript(source: String) {
             .start()
 
         // Reading the output stream
-        val outputReader = processScript.inputReader()
-        val line = StringBuilder()
+        processScript.inputReader().use {
+            val line = StringBuilder()
+            var nextChar: Char
+            while (it.read().apply { nextChar = this.toChar() } != -1) {
+                print(nextChar) // TODO line is here only for debug purposes, remember to remove it.
+                line.append(nextChar)
 
-        do {
-            val nextVal = outputReader.read()
-            val nextChar = nextVal.toChar()
-
-            print(nextChar) // TODO line is here only for debug purposes, remember to remove it.
-            line.append(nextChar)
-
-            // Checking line
-            if (nextChar == '\n' || nextChar == '\r') {
-                parseLine(line.toString())
-                line.clear()
+                // Checking line
+                if (nextChar == '\n' || nextChar == '\r') {
+                    // Parsing line
+                    val lineType = parseLine(line.toString())
+                    if (lineType == LineType.ERROR) {
+                        // TODO Update the terminalLabel to handle the case of error
+                    } else if (lineType == LineType.EXCEPTION) {
+                        // TODO handle the terminalLabel to handle the case of exception
+                    }
+                    line.clear()
+                }
             }
-
-        } while (nextVal != -1)
+        }
 
         // Check existStatus
-        println("---EXITING...---")
+        println("\n---EXITING...---")
         println("process ${processScript.pid()} with exit status ${processScript.exitValue()}")
     } catch (ioExc: IOException) {
         val missingCompilerRegex = Regex("Cannot run program \"\\w+\": error=\\d+, No such file or directory")
@@ -49,16 +57,16 @@ fun executeScript(source: String) {
 /**
  * Checks if [line] is an error or an exception.
  */
-fun parseLine(line: String) {
+fun parseLine(line: String): LineType? {
     val errorRegex = Regex("(\\w*/*)*\\w+\\.kts:\\d+:\\d+: error: .*([\\n\\r])*")
     val exceptionRegex = Regex("java\\.lang\\..+: .+([\\n\\r])*")
 
     if (errorRegex.matches(line)) {
-        println("FOUND AN ERROR!")
+        return LineType.ERROR
     } else if (exceptionRegex.matches(line)) {
-        println("FOUND AN EXCEPTION!")
+        return LineType.EXCEPTION
     }
-
+    return null
 }
 
 fun main() {
