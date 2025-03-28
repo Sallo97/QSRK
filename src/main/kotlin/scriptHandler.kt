4cabb42e6a1c4eb681b8a7e426843ea801c4aa9e
Sal <salerno.salvatore1997@gmail.com>
2025-03-28 15:13:04 +0100

    Handled try-catch to handle IOExceptions when creating the process

--- src/main/kotlin/scriptHandler.kt
+++ src/main/kotlin/scriptHandler.kt
@@ -1,3 +1,5 @@
+import java.io.IOException
+
 /**
  * This file handle the execution of the written script, creating the associated process, parsing the output and checking
  * the process's termination.
@@ -8,29 +10,36 @@
  */
 fun executeScript(source: String) {
     // Initialising process
-    val processScript = ProcessBuilder("kotlinc", "-script", source)
-        .redirectErrorStream(true)
-        .redirectInput(ProcessBuilder.Redirect.INHERIT)
-        .start()
+    try {
+        val processScript = ProcessBuilder("kotlinc", "-script", source)
+            .redirectErrorStream(true)
+            .redirectInput(ProcessBuilder.Redirect.INHERIT)
+            .start()
 
-    // Reading the output stream
-    val outputReader = processScript.inputReader()
-    val line = StringBuilder()
+        // Reading the output stream
+        val outputReader = processScript.inputReader()
+        val line = StringBuilder()
 
-    do {
-        val nextVal = outputReader.read()
-        val nextChar = nextVal.toChar()
+        do {
+            val nextVal = outputReader.read()
+            val nextChar = nextVal.toChar()
 
-        print(nextChar) // TODO line is here only for debug purposes, remember to remove it.
-        line.append(nextChar)
+            print(nextChar) // TODO line is here only for debug purposes, remember to remove it.
+            line.append(nextChar)
 
-        // Checking line
-        if (nextChar == '\n' || nextChar == '\r') {
-            parseLine(line.toString())
-            line.clear()
-        }
+            // Checking line
+            if (nextChar == '\n' || nextChar == '\r') {
+                parseLine(line.toString())
+                line.clear()
+            }
 
-    } while (nextVal != -1)
+        } while (nextVal != -1)
+    } catch (ioExc: IOException) {
+        val missingCompilerRegex = Regex("Cannot run program \"\\w+\": error=\\d+, No such file or directory")
+        if (ioExc.message != null && missingCompilerRegex.matches(ioExc.message!!))
+            println("Your system misses the Kotlin compiler, please be sure that you installed kotlinc")
+        else throw ioExc
+    }
 }
 
 /**
