9fa33b8879494a74f2814387fbe64c57f25ab8c6
Sal <salerno.salvatore1997@gmail.com>
2025-04-07 18:08:03 +0200

    Moved the field handling script output to its own file.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -11,9 +10,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.input.key.*
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.TextLayoutResult
-import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.TextFieldValue
@@ -22,7 +19,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import guiComponents.buttons.playButton
 import guiComponents.buttons.stopButton
-import parsing.errorParsing.ErrorTransformation
+import guiComponents.fields.outputField
 import parsing.syntaxParsing.SyntaxTransformation
 import scriptHandler.ScriptStatus
 
@@ -42,11 +39,11 @@ fun LineNumbers.addLine(){
 fun App() {
 
     MaterialTheme {
-        var script by remember { mutableStateOf(TextFieldValue(text = "Write your script here!")) }
+        val script = remember { mutableStateOf(TextFieldValue(text = "Write your script here!")) }
         val output = remember { mutableStateOf("The output of you script will be printed here!") }
         val status = remember { mutableStateOf(ScriptStatus()) }
         val currentProcess: MutableState<Process?> = remember { mutableStateOf(null) }
-        var textLayoutResult by remember { mutableStateOf<TextLayoutResult?>(null) }
+        val textLayoutResult = remember { mutableStateOf<TextLayoutResult?>(null) }
 
         val textStyle = TextStyle(color = Color.LightGray, fontFamily = FontFamily.Monospace)
 
@@ -89,8 +86,8 @@ fun App() {
 
                             // ScriptBox
                             BasicTextField(
-                                value = script,
-                                onValueChange = { script = it },
+                                value = script.value,
+                                onValueChange = { script.value = it },
                                 visualTransformation = SyntaxTransformation,
                                 textStyle = textStyle,
                                 modifier = Modifier
@@ -121,51 +118,11 @@ fun App() {
                             .background(MyColors.fieldBackground, shape = RoundedCornerShape(10.dp))
                             .padding(horizontal = 12.dp, vertical = 8.dp)
                     ) {
-                        BasicTextField(
-                            onTextLayout = {
-                                textLayoutResult = it
-                            },
-                            enabled = false,
-                            readOnly = true,
-                            visualTransformation = ErrorTransformation,
-                            value = output.value,
-                            textStyle = textStyle,
-                            onValueChange = { },
-                            modifier = Modifier
-                                .background(color = MyColors.fieldBackground)
-                                .border(
-                                    width = 2.dp,
-                                    brush = SolidColor(MyColors.fieldBorder),
-                                    shape = RectangleShape
-                                )
-                                .fillMaxWidth(0.85f)
-                                .fillMaxHeight(0.8f)
-                                .verticalScroll(rememberScrollState())  // Enable scrolling
-                                .pointerInput(Unit) {
-                                    detectTapGestures { tapOffset ->
-                                        textLayoutResult?.let { it ->
-                                            val clickedCharOffset = it.getOffsetForPosition(tapOffset)
-                                            ErrorTransformation.currentAnnotatedString?.let { annotatedString ->
-                                                annotatedString.getStringAnnotations(
-                                                    tag = "CLICKABLE",
-                                                    start = clickedCharOffset,
-                                                    end = clickedCharOffset
-                                                ).firstOrNull()?.let {
-                                                    // Parsing tag to retrieve row and col
-                                                    val listError = it.item.split(":").map { str ->
-                                                        str.toInt()
-                                                    }
-                                                    // Determine and setting cursor position
-                                                    val row = listError.first()
-                                                    val col = if (listError.size == 2) (listError[1]) else 0
-                                                    val cursorOffset = findCursorPosition(row, col, script.text)
-                                                    script =
-                                                        script.copy(selection = TextRange(cursorOffset, cursorOffset))
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
+                        outputField(
+                            textLayoutResult,
+                            output,
+                            textStyle,
+                            script
                         )
                     }
                 }
@@ -174,19 +131,16 @@ fun App() {
 
                 Column {
                     Spacer(Modifier.height(10.dp))
-
                     playButton(
                         status,
                         scope,
                         output,
                         currentProcess,
-                        script
+                        script.value
                     )
                     Spacer(Modifier.height(10.dp))
                     stopButton(scope,currentProcess)
                     Spacer(Modifier.height(10.dp))
-
-                    // StatusIcon
                     Icon(
                         imageVector = status.value.icon,
                         contentDescription = "status Icon",
@@ -200,21 +154,7 @@ fun App() {
     }
 }
 
-/**
- * Given the [row] and [col] of an error description referring to [text], returns the exact cursor position the error
- * refers to
- */
-private fun findCursorPosition(row: Int, col: Int = 0, text: String): Int {
-    // Retrieving actual position
-    val realRow = row - 1
-    val realCol = col - 1
-    var offsetRow = 0
-    val lines = text.lines()
-    for (i in 0..<realRow)
-        offsetRow += lines[i].length + 1
-    return offsetRow + realCol
 
-}
 
 fun main() =
     application {
---
+++ src/main/kotlin/guiComponents/fields/outputField.kt
@@ -0,0 +1,93 @@
+package guiComponents.fields
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import parsing.errorParsing.ErrorTransformation
+
+@Composable
+fun outputField(
+    textLayoutResult: MutableState<TextLayoutResult?>,
+    output: MutableState<String>,
+    textStyle: TextStyle,
+    script: MutableState<TextFieldValue>
+){
+    BasicTextField(
+        onTextLayout = {
+            textLayoutResult.value = it
+        },
+        enabled = false,
+        readOnly = true,
+        visualTransformation = ErrorTransformation,
+        value = output.value,
+        textStyle = textStyle,
+        onValueChange = { },
+        modifier = Modifier
+            .background(color = MyColors.fieldBackground)
+            .border(
+                width = 2.dp,
+                brush = SolidColor(MyColors.fieldBorder),
+                shape = RectangleShape
+            )
+            .fillMaxWidth(0.85f)
+            .fillMaxHeight(0.8f)
+            .verticalScroll(rememberScrollState())  // Enable scrolling
+            .pointerInput(Unit) {
+                detectTapGestures { tapOffset ->
+                    textLayoutResult.let { it ->
+                        val clickedCharOffset = it.value!!.getOffsetForPosition(tapOffset)
+                        ErrorTransformation.currentAnnotatedString?.let { annotatedString ->
+                            annotatedString.getStringAnnotations(
+                                tag = "CLICKABLE",
+                                start = clickedCharOffset,
+                                end = clickedCharOffset
+                            ).firstOrNull()?.let {
+                                // Parsing tag to retrieve row and col
+                                val listError = it.item.split(":").map { str ->
+                                    str.toInt()
+                                }
+                                // Determine and setting cursor position
+                                val row = listError.first()
+                                val col = if (listError.size == 2) (listError[1]) else 0
+                                val cursorOffset = findCursorPosition(row, col, script.value.text)
+                                script.value =
+                                    script.value.copy(selection = TextRange(cursorOffset, cursorOffset))
+                            }
+                        }
+                    }
+                }
+            }
+    )
+}
+
+/**
+ * Given the [row] and [col] of an error description referring to [text], returns the exact cursor position the error
+ * refers to
+ */
+private fun findCursorPosition(row: Int, col: Int = 0, text: String): Int {
+    // Retrieving actual position
+    val realRow = row - 1
+    val realCol = col - 1
+    var offsetRow = 0
+    val lines = text.lines()
+    for (i in 0..<realRow)
+        offsetRow += lines[i].length + 1
+    return offsetRow + realCol
+
+}
